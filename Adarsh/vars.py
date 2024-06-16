6437bce152108c253572347eee72adf94d1a9662
senraju35538 <senraju35538@gmail.com>
2024-06-16 10:12:44 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -24,7 +24,7 @@ class Var(object):
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5274042199").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'hot_animal_1'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'hot_animal'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
